50c7f70949beb0db8e757ae8f451994b7fe69d58
e531-devflops <e531.devflops@gmail.com>
2021-02-25 15:09:48 +0900

    initial commit

---
+++ src/test/java/kr/daoko/kafka/SpringKafkaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.daoko.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
